43513cf7f5e8f09895ee3de1253588cce7dd60a3
thearjun <mailarjunadhikari@gmail.com>
2019-10-19 15:22:17 +0545

    Database model for Daily Diary

--- entry/models.py
+++ entry/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class DiaryModel(models.Model):
+    note = models.CharField(max_length=100)
+    content = models.TextField()
+    posted_date = models.DateTimeField()
+
+    def posted_date_preety(self):
+        return self.posted_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.note
+
+    def summary(self):
+        if len(self.note) > 100:
+            return self.note[:100] + '...'
+        return self.note[:100]
