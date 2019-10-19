3e083a07a114bf451554c67425f16f5966f5d331
thearjun <mailarjunadhikari@gmail.com>
2019-10-19 15:22:35 +0545

    Added Note in Forms

--- entry/forms.py
+++ entry/forms.py
@@ -2,6 +2,17 @@ from django import forms
 
 
 class AddForm(forms.Form):
+    note = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Name it something',
+                'class': " form-control mb-3",
+            }
+        ),
+        label='',
+        required=True
+    )
+
     content = forms.CharField(
         widget=forms.Textarea(
             {
